f9f307411e8e4e6a6ec65b80e307f4ee6e0cf779
TC Twelker <timothy.twelker@gmail.com>
2024-01-15 16:14:11 -0700

    Chat working

--- main.go
+++ main.go
@@ -1,3 +1,13 @@
+package main
+
+import (
+	"github.com/gorilla/websocket"
+	"github.com/satori/go.uuid"
+	"net/http"
+	"fmt"
+	"encoding/json"
+)
+
 type ClientManager struct {
     clients    map[*Client]bool
     broadcast  chan []byte
@@ -86,7 +96,7 @@ func (c *Client) write() {
 		select {
 		case message, ok := <-c.send:
 			if !ok {
-				c.socket.WriteMessage(websocket.CloseMessage, [byte{}])
+				c.socket.WriteMessage(websocket.CloseMessage, []byte{})
 				return
 			}
 
@@ -94,3 +104,25 @@ func (c *Client) write() {
 		}
 	}
 }
+
+func wsPage(res http.ResponseWriter, req *http.Request) {
+	conn, error := (&websocket.Upgrader{CheckOrigin: func(r *http.Request) bool { return true}}).Upgrade(res, req, nil)
+	if error != nil {
+		http.NotFound(res, req)
+		return
+	}
+	client := &Client{id: uuid.NewV4().String(), socket: conn, send: make(chan []byte)}
+
+	manager.register <- client
+
+	go client.read()
+	go client.write()
+}
+
+func main() {
+	fmt.Println("Starting application...")
+	go manager.start()
+	http.HandleFunc("/ws", wsPage)
+	http.ListenAndServe(":12345", nil)
+}
+
